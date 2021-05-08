9ad6e67e002ab2da6af44cea137cf3591e0d7d2e
李光浩 <liguanghao@viatomtech.com>
2021-05-08 11:19:49 +0800

    好了， 可以读白卡了 但是那个那个啊， 那个ndef还不行呢

--- app/src/main/java/com/vaca/mynfc/MActivity.kt
+++ app/src/main/java/com/vaca/mynfc/MActivity.kt
@@ -5,37 +5,17 @@ import android.nfc.*
 import android.nfc.tech.Ndef
 import android.nfc.tech.NdefFormatable
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import java.io.IOException
 import java.nio.charset.Charset
 import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+class MActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-//--------------------------------------------------tnf_absolute_uri类型的数据
-        val uriRecord = NdefRecord(
-            NdefRecord.TNF_ABSOLUTE_URI,
-            "http://developer.android.com/index.html".toByteArray(Charset.forName("US-ASCII")),
-            ByteArray(0),
-            ByteArray(0)
-        )
-//----------------------------------------------------------------tnf_mime_media类型的数据
-        val mimeRecord = NdefRecord.createMime(
-            "application/vnd.com.example.android.beam",
-            "Beam me up, Android".toByteArray(Charset.forName("US-ASCII"))
-        )
-
-//--------------------------------------------------------------tnf_external_type类型的数据
-        var payload: ByteArray= byteArrayOf(5,6,7,8)
-        val domain = "com.example" //usually your app's package name
-        val type = "externalType"
-        val extRecord = NdefRecord.createExternal(domain, type, payload)
-
-
+        setContentView(R.layout.activity_m)
 
 
     }
